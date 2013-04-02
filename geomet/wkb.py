9d8f4b7e25fa9fd5874d89d65245cf9ceac91b85
Lars Butler <Lars.Butler@gmail.com>
2013-04-02 09:34:39 +0200

    wkb:
    Sketched out the API for the WKB/GeoJSON conversion.

---
+++ geomet/wkb.py
@@ -0,0 +1,20 @@
+def dump(obj, dest_file):
+    pass
+
+
+def load(source_file):
+    pass
+
+
+def dumps(obj, decimals=16):
+    """
+    Dump a GeoJSON-like `dict` to a WKB string.
+    """
+    pass
+
+
+def loads(string):
+    """
+    Construct a GeoJson `dict` from WKB (`string`).
+    """
+    pass
