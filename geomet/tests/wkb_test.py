ad863490012e5ba9b31419fde90c6d2ddd1c70c7
Lars Butler <Lars.Butler@gmail.com>
2013-05-13 21:11:08 +0200

    tests/wkb_test:
    Removed an unnecessary test case.

--- geomet/tests/wkb_test.py
+++ geomet/tests/wkb_test.py
@@ -47,26 +47,6 @@ class PointTestCase(unittest.TestCase):
         )
         self.assertEqual(expected, wkb.dumps(pt, big_endian=True))
 
-    # We skip this because, right now, we have no way of signalling with
-    # GeoJSON that an object with 3-dimensional coordinates is XYZ or XYM.
-    # XYZ is more common, so we assume this.
-    @unittest.skip
-    def test_dumps_point_m(self):
-        # Test for an XYM Point:
-        pt = dict(type='Point', coordinates=[0.0, 1.0, 2.0])
-
-        data = ('\x00\x00\x00\x00\x00\x00\x00\x00'
-                '\x00\x00\x00\x00\x00\x00\xf0?'
-                '\x00\x00\x00\x00\x00\x00\x00@')
-
-        expected = EXP_WKB_FMT
-        expected %= dict(
-            endian='\x01',
-            type='\x01\x20\x00\x00',  # M type geometry (X, Y, M)
-            data=data,
-        )
-        self.assertEqual(expected, wkb.dumps(pt, big_endian=False))
-
     def test_dumps_point_4d(self):
         # Test for an XYZM Point:
         pt = dict(type='Point', coordinates=[0.0, 1.0, 2.0, 4.0])
