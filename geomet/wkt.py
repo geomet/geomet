97c6e473358d193320b5a863bd3be9efb987f0b8
Lars Butler <Lars.Butler@gmail.com>
2013-05-13 13:32:20 +0200

    wkt:
    Added `__tokenize_wkt` to treat "-" and the subsequent numeric value as
    a single token.

--- geomet/wkt.py
+++ geomet/wkt.py
@@ -61,6 +61,26 @@ def loads(string):
     return importer(tokens, string)
 
 
+def __tokenize_wkt(tokens):
+    """
+    Since the tokenizer treats "-" and numeric strings as separate values,
+    combine them and yield them as a single token. This utility encapsulates
+    parsing of negative numeric values from WKT can be used generically in all
+    parsers.
+    """
+    negative = False
+    for t in tokens:
+        if t == '-':
+            negative = True
+            continue
+        else:
+            if negative:
+                yield '-%s' % t
+            else:
+                yield t
+            negative = False
+
+
 def __unsupported_geom_type(geom_type):
     raise ValueError("Unsupported geometry type '%s'" % geom_type)
 
