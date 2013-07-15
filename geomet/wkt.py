9e3857929d06892fc902b07bdb8fad8eca9b8e89
Lars Butler <Lars.Butler@gmail.com>
2013-07-15 23:44:20 +0200

    wkt:
    Added __dump_polygon implementation.

--- geomet/wkt.py
+++ geomet/wkt.py
@@ -117,8 +117,21 @@ def __dump_linestring(obj, fmt):
     return ls
 
 
-def __dump_polygon(obj, decimals):
-    raise NotImplementedError
+def __dump_polygon(obj, fmt):
+    """
+    Dump a GeoJSON-like Polygon object to WKT.
+
+    Input parameters and return value are the POLYGON equivalent to
+    :func:`__dump_point`.
+    """
+    coords = obj['coordinates']
+    poly = 'POLYGON (%s)'
+    rings = (', '.join(' '.join(fmt % c for c in pt) for pt in ring)
+             for ring in coords)
+    rings = ('(%s)' % r for r in rings)
+    rings = ', '.join(rings)
+    poly %= rings
+    return poly
 
 
 def __dump_multipoint(obj, decimals):
