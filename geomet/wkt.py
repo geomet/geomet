f90fbea2c349848278736d916cefbf7a3b5e35ba
Lars Butler <Lars.Butler@gmail.com>
2013-10-16 13:36:33 +0200

    wkt:
    Implemented __dump_geometrycollection.

--- geomet/wkt.py
+++ geomet/wkt.py
@@ -199,7 +199,23 @@ def __dump_multipolygon(obj, fmt):
 
 
 def __dump_geometrycollection(obj, fmt):
-    raise NotImplementedError
+    """
+    Dump a GeoJSON-like GeometryCollection object to WKT.
+
+    Input parameters and return value are the GEOMETRYCOLLECTION equivalent to
+    :func:`__dump_point`.
+
+    The WKT conversions for each geometry in the collection are delegated to
+    their respective functions.
+    """
+    gc = 'GEOMETRYCOLLECTION (%s)'
+    geoms = obj['geometries']
+    geoms_wkt = []
+    for geom in geoms:
+        geom_type = geom['type']
+        geoms_wkt.append(__dumps_registry.get(geom_type)(geom, fmt))
+    gc %= ','.join(geoms_wkt)
+    return gc
 
 
 __dumps_registry = {
