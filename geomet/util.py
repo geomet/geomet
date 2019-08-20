918531a1ba621072b69276ffa69743b69d074fe4
Lars Butler <Lars.Butler@gmail.com>
2019-08-21 00:15:16 +0200

    Fix an import deprecation warning for `collections`
    After pyton3.7, one must use `collections.abc.Iterable`, for example,
    instead of `collections.Iterable`.

--- geomet/util.py
+++ geomet/util.py
@@ -11,9 +11,12 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import collections
 import itertools
 import six
+if six.PY2:
+    import collections
+else:
+    import collections.abc as collections
 
 
 def block_splitter(data, block_size):
