6db8771889a79df30da39920cae5f8de75f4527c
Tom Caruso <carusot42@gmail.com>
2020-03-16 01:15:23 -0400

    adding some envelope handling

--- geomet/tests/test_geopackage.py
+++ geomet/tests/test_geopackage.py
@@ -18,9 +18,27 @@ from geomet import geopackage
 
 
 class TestGeoPackageLoads(unittest.TestCase):
-    def test_loads_noenvelope_with_srid(self):
-        gpkg = b'GP\x00\x01\x11\x0f\x00\x00\x01\x01\x00\x00\x00\x16\xfb\xcb&\xe5\xd1W\xc1\x80?5.\xb4\xdb:\xc1'
-        expected = {'type': 'Point', 'coordinates': [-6244244.6062, -1760180.1805000007], 'meta': {'srid': 3857}, 'crs': {'type': 'name', 'properties': {'name': 'EPSG3857'}}}
+    def test_loads_2d_noenvelope_with_srid(self):
+        gpkg = (  # GPKG header
+            b'GP'  # "magic"
+            b'\x00'  # version
+            b'\x01'  # flags
+            b'\xe6\x10\x00\x00'  # SRID
+            b'\x01\x01\x00\x00\x00\xf0\x9e\xa0\xa7\x05;#@hZ\xbd\x93\x83GC@'  # WKB geom
+        )
+        expected = {'type': 'Point', 'coordinates': [9.615277517659223, 38.55870291467437], 'meta': {'srid': 4326}, 'crs': {'type': 'name', 'properties': {'name': 'EPSG4326'}}}
 
         self.assertEqual(expected, geopackage.loads(gpkg))
 
+    def test_loads_2d_envelope_with_srid(self):
+        gpkg = (
+            b'GP'
+            b'\x00'
+            b'\x02'
+            b'\xe6\x10\x00\x00' 
+            b'\xf0\x9e\xa0\xa7\x05;#@hZ\xbd\x93\x83GC@\xf0\x9e\xa0\xa7\x05;#@hZ\xbd\x93\x83GC@'  # Envelope
+            b'\x01\x01\x00\x00\x00\xf0\x9e\xa0\xa7\x05;#@hZ\xbd\x93\x83GC@'  # WKB
+        )
+        expected = {'type': 'Point', 'coordinates': [9.615277517659223, 38.55870291467437], 'meta': {'srid': 4326, 'envelope': (9.615277517659223, 38.55870291467437, 9.615277517659223, 38.55870291467437)}, 'crs': {'type': 'name', 'properties': {'name': 'EPSG4326'}}}
+
+        self.assertEqual(expected, geopackage.loads(gpkg))
