523684c306e5ce4c04443e5d590f791461f541b0
Lars Butler <Lars.Butler@gmail.com>
2022-08-07 16:31:04 -0600

    Update version to 0.3.1 for release

--- geomet/__init__.py
+++ geomet/__init__.py
@@ -11,7 +11,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 
 class InvalidGeoJSONException(Exception):
